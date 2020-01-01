d81593db0255d78a9d4d82ca7a1c910256d66570
siningtong <siningtong@gmail.com>
2020-01-01 05:27:33 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import RollDice from "./RollDice";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return <RollDice />;
+  }
+}
+
+export default App;
---
+++ src/RollDice.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import './App.css';
+
+class RollDice extends Component {
+  render() {
+    return (
+     <div>HI</div>
+    );
+  }
+}
+
+export RollDice App;
